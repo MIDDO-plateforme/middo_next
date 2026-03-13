4a23006de0ed941438bc304f856f8c7e31d39ac5
Koba <mbane_lokotambane_lokota@gmail.com>
2026-03-13 18:54:53 +0100

    Add Domain module structure + Middo entity

---
+++ src/Domain/Entity/Middo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\Entity;
+
+class Middo
+{
+    private string $id;
+    private string $name;
+
+    public function __construct(string $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+}
