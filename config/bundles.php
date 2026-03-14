03d6f1c35ced97b9d3037edb5178351f4a1f97a9
Koba Lokota <bmbane@yahoo.fr>
2026-03-14 13:42:05 +0100

    Fix DebugBundle and WebProfilerBundle for production

--- config/bundles.php
+++ config/bundles.php
@@ -4,13 +4,21 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+
+    // DebugBundle uniquement en dev et test
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+
+    // WebProfiler uniquement en dev et test
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+
+    // MakerBundle uniquement en dev
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
