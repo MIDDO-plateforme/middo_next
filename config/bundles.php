3084e21d13adf1aa6a3147553c92ac2eeb913943
Koba Lokota <bmbane@yahoo.fr>
2026-03-15 07:55:33 +0100

    Remove DebugBundle and WebProfilerBundle from bundles.php for production

--- config/bundles.php
+++ config/bundles.php
@@ -5,13 +5,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 
-    // DebugBundle uniquement en dev et test
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-
-    // WebProfiler uniquement en dev et test
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
@@ -19,6 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 
-    // MakerBundle uniquement en dev
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
